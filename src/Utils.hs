aef9df548163149a282203015cde2bb399be18f8
Nick Straw <nick@broxholme.com>
2022-01-09 22:48:01 +0000

    Finished, I think

--- src/Utils.hs
+++ src/Utils.hs
@@ -8,15 +8,12 @@ module Utils (
   , getRaw
   , getWords
   , splitOn
-  , toInt
-  , fromInt
-  , pad
   , Coord
   , allCoords
   , neighbourCoords4
   , neighbourCoords8
   , splitAt
-  , intersections
+  --, intersections
   , clockTurn
   , antiTurn
   , directions8
@@ -28,7 +25,6 @@ module Utils (
   , neighbours4
   , neighbours8
   , span
-  , race
   , chunksOf
   , first
   , second
@@ -59,21 +55,18 @@ module Utils (
   , isJust
   , intersect
   , isNothing
-  , safeHead
   , delete
   , replace
   , trace
   , Semigroup(..)
   , Monoid(..)
   , bool
-  , fix
   , timeIt
 )
 where
 
 
 import qualified Data.Set as S
---import qualified Data.HashSet as HS
 import qualified Data.Vector as V
 import qualified Data.Map.Strict as M
 import qualified Data.IntMap.Strict as IM
@@ -91,7 +84,7 @@ import System.TimeIt ( timeIt )
 import Data.Semigroup (Semigroup(..))
 import Data.Monoid (Monoid(..))
 import Debug.Trace (trace)
-import Data.Bool
+import Data.Bool (bool)
 
 --- Things to add
 
@@ -104,6 +97,7 @@ getF :: (String -> a) -> Int -> IO a
 getF f n = do
   s <- readFile $ "./Data/Day" ++ show n ++ ".in"
   return $ f s
+
 
 getT :: (String -> a) -> Int -> IO a
 getT f n = do
@@ -126,15 +120,8 @@ getLines = getF lines
 getTest :: Int -> IO [String]
 getTest = getT lines
 
+
 ------------------ VARIOUS UTILITY FUNCTIONS --------------------
-
-
-intersections :: Ord a => [S.Set a] -> S.Set a
-intersections ss = foldl' S.intersection (head ss) (tail ss)
-
-
-fix :: Eq a => (a -> a) -> a
-fix f = x where x = f x
 
 
 steadyState :: Eq a => (a -> a) -> a -> a
@@ -147,59 +134,9 @@ steadyState' f x = case f x of
                      Just y -> steadyState' f y
 
 
--- This takes 2 predicates isFinished and isOK
--- and a start value and a next function
--- it returns True if isOK happens before isFinished
-race :: (a -> Bool) -> (a -> Bool) -> a -> (a -> a) -> Bool
-race isFinished isOk x next 
-  | isFinished nxt = False
-  | isOk nxt = True
-  | otherwise = race isFinished isOk nxt next
-  where
-    nxt = next x
-
-
-------------------- BOOLEAN / BINARY TO/FROM INT
-
--- This does conversion units at the front of the list
-toInt :: [Bool] -> Integer
-toInt [] = 0
-toInt bs = 2 * toInt (tail bs) + if head bs then 1 else 0
-
-
-fromInt :: Integer -> [Bool]
-fromInt 0 = [False]
-fromInt i = helper i
-  where
-    helper 0 = []
-    helper i = let (q,r) = i `divMod` 2 in (r==1) : helper q
-
-
-pad :: Integer -> a -> [a] -> [a]
-pad n b bs = replicate (fromIntegral n - length bs) b ++ take (fromIntegral n) bs
-
-
------------------------- SPLITTING OF STRINGS -----------------------
-
-
-
--- Like words but you specify the character - very slow, I expect
-splitOnChar :: Char -> String -> [String]
-splitOnChar c s = (go <$>) <$> words tc
-  where
-    -- turn c into space and space into c
-    go :: Char -> Char
-    go x
-      | x == c = ' '
-      | x == ' ' = c
-      | otherwise = x
-    tc = go <$> s
-
 
 ------------------------ COORDINATE / VECTOR STUFF ------------
 
--- Traditionally measured down from top left
--- Coords are (x, y)
 
 type Coord = (Int, Int)
 
@@ -211,9 +148,6 @@ instance Num Coord where
   abs (x, y) = (abs x, abs y)
   signum (x, y) = (signum x, signum y)
   fromInteger i = (fromInteger i, 0)
-
-
-largePrime = 11939
 
 
 manhattan :: Coord -> Coord -> Int
@@ -246,13 +180,8 @@ neighbours8 :: Coord -> [Coord]
 neighbours8 c = neighbourCoords8 `at` c
 
 
-
 at :: [Coord] -> Coord -> [Coord]
 coords `at` origin = map (+ origin) coords
-
-
-mul :: Int -> Coord -> Coord
-mul c (x,y) = (c*x, c*y)
 
 
 -- All coords in a grid in (x, y) (col, row) order
@@ -262,6 +191,7 @@ allCoords rows cols = concat $ (\c -> (c,) <$> [0..(rows-1)]) <$> [0..(cols-1)]
 
 directions8 :: [Coord]
 directions8 = [(0, -1), (0, 1), (1, 0), (-1, 0), (1, -1), (1, 1), (-1, 1), (-1, -1)]
+
 
 directions4 :: [Coord]
 directions4 = [(0, -1), (0, 1), (1, 0), (-1, 0)]
@@ -274,8 +204,4 @@ rightOf x = x + (1,0)
 above x = x + (0,-1)
 below x = x + (0,1)
 
-
-safeHead :: [a] -> Maybe a
-safeHead [] = Nothing 
-safeHead (x:_) = Just x
 
